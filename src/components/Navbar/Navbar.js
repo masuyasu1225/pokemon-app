0734d3e22382e26027afb1d8148dad26fe9d5a1c
masu <yasumoto.7991mm@gmail.com>
2023-07-10 09:49:30 +0700

    update navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <nav>ポケモン図鑑</nav>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="navbar-container">
+      <nav>ポケモン図鑑</nav>
+      <button
+        className={`hamburger-menu ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+      </button>
+      {isOpen && (
+        <ul className="menu">
+          <li>メニュー1</li>
+          <li>メニュー2</li>
+          <li>メニュー3</li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
